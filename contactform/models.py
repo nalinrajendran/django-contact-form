b59020760a64b15d01b55aa77d75426adc679b11
Aadharsh Aadhithya <38402714+AadharshAadhithya@users.noreply.github.com>
2022-09-09 23:51:48 +0530

    Dockerizing

--- contactform/models.py
+++ contactform/models.py
@@ -4,31 +4,27 @@ from django.db import models
 # Create your models here.
 
 
-
 class Contact(models.Model):
 
-
     SKILL_CHOICES = (
-    ('Beginner','BEGINNER'),
-    ('Intermediate', 'INTERMEDIATE'),
-    ('Pro','PRO'),
+        ('Beginner', 'BEGINNER'),
+        ('Intermediate', 'INTERMEDIATE'),
+        ('Pro', 'PRO'),
     )
 
     name = models.CharField(max_length=100)
-    rollno = models.TextField()
+    rollno = models.TextField(primary_key=True, unique=True)
     mail = models.EmailField()
-    skill = models.CharField(max_length=15, choices=SKILL_CHOICES, default='Beginner')
+    skill = models.CharField(
+        max_length=15, choices=SKILL_CHOICES, default='Beginner')
 
     def __str__(self):
 
         return self.name
 
 
-    
-
-
 class Post(models.Model):
     text = models.TextField()
 
-    def __str__ (self):
+    def __str__(self):
         return self.text[:50]
